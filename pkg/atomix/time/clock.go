3bdeadedbaddf3d87f0508797ba9fb38eb1d0872
Jordan Halterman <jordan.halterman@gmail.com>
2021-01-23 00:22:48 -0800

    Add Get/Update methods to clocks

--- pkg/atomix/time/clock.go
+++ pkg/atomix/time/clock.go
@@ -21,31 +21,73 @@ import (
 
 // Clock is an interface for clocks
 type Clock interface {
+	// Get gets the current timestamp
+	Get() Timestamp
 	// Increment increments the clock
 	Increment() Timestamp
+	// Update updates the timestamp
+	Update(Timestamp) Timestamp
 }
 
 // NewPhysicalClock creates a new physical clock
 func NewPhysicalClock() Clock {
-	return &PhysicalClock{}
+	return &PhysicalClock{
+		timestamp: NewPhysicalTimestamp(PhysicalTime(time.Now())),
+	}
 }
 
 // PhysicalClock is a clock that produces PhysicalTimestamps
-type PhysicalClock struct{}
+type PhysicalClock struct {
+	timestamp Timestamp
+	mu        sync.RWMutex
+}
+
+func (c *PhysicalClock) Get() Timestamp {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.timestamp
+}
 
 func (c *PhysicalClock) Increment() Timestamp {
-	return NewPhysicalTimestamp(PhysicalTime(time.Now()))
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.timestamp = NewPhysicalTimestamp(PhysicalTime(time.Now()))
+	return c.timestamp
+}
+
+func (c *PhysicalClock) Update(update Timestamp) Timestamp {
+	c.mu.RLock()
+	current := c.timestamp
+	c.mu.RUnlock()
+	if !update.After(current) {
+		return current
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if update.After(c.timestamp) {
+		c.timestamp = update
+	}
+	return c.timestamp
 }
 
 // NewLogicalClock creates a new logical clock
 func NewLogicalClock() Clock {
-	return &LogicalClock{}
+	return &LogicalClock{
+		timestamp: NewLogicalTimestamp(LogicalTime(0)).(LogicalTimestamp),
+	}
 }
 
 // LogicalClock is a clock that produces LogicalTimestamps
 type LogicalClock struct {
 	timestamp LogicalTimestamp
-	mu        sync.Mutex
+	mu        sync.RWMutex
+}
+
+func (c *LogicalClock) Get() Timestamp {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.timestamp
 }
 
 func (c *LogicalClock) Increment() Timestamp {
@@ -58,22 +100,73 @@ func (c *LogicalClock) Increment() Timestamp {
 	return timestamp
 }
 
+func (c *LogicalClock) Update(t Timestamp) Timestamp {
+	update, ok := t.(LogicalTimestamp)
+	if !ok {
+		panic("not a logical timestamp")
+	}
+
+	c.mu.RLock()
+	current := c.timestamp
+	c.mu.RUnlock()
+	if !update.After(current) {
+		return current
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if update.After(c.timestamp) {
+		c.timestamp = update
+	}
+	return c.timestamp
+}
+
 // NewCompositeClock creates a new composite clock
 func NewCompositeClock(clocks ...Clock) Clock {
+	timestamps := make([]Timestamp, len(clocks))
+	for i, clock := range clocks {
+		timestamps[i] = clock.Get()
+	}
 	return &CompositeClock{
-		Clocks: clocks,
+		clocks:    clocks,
+		timestamp: NewCompositeTimestamp(timestamps...),
 	}
 }
 
 // CompositeClock is a clock that produces CompositeTimestamps
 type CompositeClock struct {
-	Clocks []Clock
+	clocks    []Clock
+	timestamp Timestamp
+	mu        sync.RWMutex
+}
+
+func (c *CompositeClock) Get() Timestamp {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.timestamp
 }
 
 func (c *CompositeClock) Increment() Timestamp {
-	timestamps := make([]Timestamp, len(c.Clocks))
-	for i, clock := range c.Clocks {
+	timestamps := make([]Timestamp, len(c.clocks))
+	for i, clock := range c.clocks {
 		timestamps[i] = clock.Increment()
 	}
 	return NewCompositeTimestamp(timestamps...)
+}
+
+func (c *CompositeClock) Update(t Timestamp) Timestamp {
+	update, ok := t.(CompositeTimestamp)
+	if !ok {
+		panic("not a composite timestamp")
+	}
+
+	timestamps := make([]Timestamp, len(c.clocks))
+	for i, clock := range c.clocks {
+		timestamps[i] = clock.Update(update.Timestamps[i])
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.timestamp = NewCompositeTimestamp(timestamps...)
+	return c.timestamp
 }
