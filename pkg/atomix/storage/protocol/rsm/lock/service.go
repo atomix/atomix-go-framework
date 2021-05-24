4f1cdb2c0c6572cba5145cd94f535b07c8278361
Jordan Halterman <jordan.halterman@gmail.com>
2021-05-23 19:18:22 -0700

    Fix nil pointer in RSM lock service

--- pkg/atomix/storage/protocol/rsm/lock/service.go
+++ pkg/atomix/storage/protocol/rsm/lock/service.go
@@ -29,6 +29,7 @@ func init() {
 func newService(context ServiceContext) Service {
 	return &lockService{
 		ServiceContext: context,
+		watchers:       make(map[ProposalID]Watcher),
 		timers:         make(map[ProposalID]rsm.Timer),
 	}
 }
