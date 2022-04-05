1a0eac3cf9a882c73658cf10ba78751cdd4a73a8
Thomas Vachuska <thomas.vachuska@gmail.com>
2022-04-04 17:53:38 -0700

    Switching license to SPDX (#17)

--- build/bin/compile_protos.sh
+++ build/bin/compile_protos.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# SPDX-FileCopyrightText: 2019-present Open Networking Foundation <info@opennetworking.org>
+#
+# SPDX-License-Identifier: Apache-2.0
 
 proto_imports="./pkg:/go/src/github.com/atomix/atomix-api/proto:${GOPATH}/src/github.com/gogo/protobuf:${GOPATH}/src/github.com/gogo/protobuf/protobuf:${GOPATH}/src"
 
